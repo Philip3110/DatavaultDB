ebe6c0c0bc33f672d51878cbb794e9558f96f53f
Philip3110 <62377349+Philip3110@users.noreply.github.com>
2020-07-17 11:34:11 +0200

    Add files via upload

---
+++ mysql/sql.py
@@ -0,0 +1,43 @@
+import mysql.connector
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas import DataFrame
+from pandastable import Table
+
+
+def get_db_data():
+    cols = ["id", "Product_Name", "Category", "Availible_Stock"]
+
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="qaz321qaz",
+        database="Panda",
+    )
+
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM products")
+    data = cursor.fetchall()
+    mydb.close()
+
+    return cols, data
+
+
+def display_plot():
+    df_cols, db_data = get_db_data()
+    data_df = pd.DataFrame(db_data, columns=df_cols).set_index("id")
+
+    data_df.plot(kind='bar', x='Product_Name', y='Availible_Stock')
+
+    plt.show()
+
+
+display_plot()
+
+# db = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="qaz321qaz",
+#         database="Panda",
+#     )
+#     cursor = db.cursor()
